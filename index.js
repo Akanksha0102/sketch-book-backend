19e47644a3e6889a4799b147b5c408a2cadf8581
Akanksha0102 <akanksha070105@gmail.com>
2025-08-05 00:55:07 +0530

    whitelisted client

--- index.js
+++ index.js
@@ -4,17 +4,30 @@ const {Server} =require("socket.io")
 const cors = require ('cors')
 
 const app = express();
-app.use(cors({origin:'http://localhost:3000'}))
+const isDev=app.settings.env === 'development'
+const URL= isDev? 'http://localhost:3000' : 'https://sketch-book-mu.vercel.app/'
+app.use(cors({origin:'URL'}))
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'URL',
     methods: ['GET', 'POST']
   }
 });
 
 io.on("connection", (socket) => {
   console.log("server connected")
+
+  socket.on('beginPath' , (arg) =>{
+    socket.broadcast.emit('beginPath ', arg)
+  })
+  socket.on('drawLine' , (arg) =>{
+    socket.broadcast.emit('drawLine ', arg)
+  })
+  socket.on('changeConfig' , (arg) =>{
+    socket.broadcast.emit('changeConfig ', arg)
+  })
+  
 });
 
 httpServer.listen(5000);
